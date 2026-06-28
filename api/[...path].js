d3dc3387349c81ac7d15b050915529bc99b6e10e
Imrana Haqiq <70132759@student.uol.edu.pk>
2026-06-28 07:03:56 +0500

    Fix Vercel API ESM import

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,10 @@
-import app from "../artifacts/api-server/dist/app.mjs";
+let appPromise;
 
-export default app;
+module.exports = async function handler(req, res) {
+  if (!appPromise) {
+    appPromise = import("../artifacts/api-server/dist/app.mjs").then((mod) => mod.default);
+  }
+
+  const app = await appPromise;
+  return app(req, res);
+};
